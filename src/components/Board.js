bfc4aa6b5866c69365f72d0acfad7d2de031c5b8
Carson Meiling <carmc93@gmail.com>
2020-07-27 21:01:18 -0600

    start of board

---
+++ src/components/Board.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import styles from '../styles/board.css';
+
+
+const Board = () => {
+
+  return(
+    <>
+    <button className='square'>
+
+    </button>
+    </>
+  )
+}
+
+export default Board;
